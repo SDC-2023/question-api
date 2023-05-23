271aca3167c6bf73ca1342360f61010ccc5b335f
Ruojia Liu <rul008@ucsd.edu>
2023-05-23 16:43:12 -0700

    setup

---
+++ server/app.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+// Serves up all static and generated assets in ../client/dist.
+
+
+/* ---------------- Server listens ---------------- */
+
+app.listen(process.env.PORT || 3000);
+console.log(`Listening at http://localhost:${process.env.PORT || 3000}`);
